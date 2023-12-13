95674dd961eed2c560af0982e104b291b083c382
Dhananjay Rokade <dhananjayrokade9665@gmail.com>
2023-12-13 23:49:57 +0530

    model training completed

--- src/component/model_trainer.py
+++ src/component/model_trainer.py
@@ -3,62 +3,91 @@ import pandas as pd
 import numpy as np
 from src.logger import logging
 from src.exception import CustmeException
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
 from dataclasses import dataclass
-from sklearn.model_selection import train_test_split
-from src.component.data_transfromation import DataTransformation
-from src.component.modrl_trainer import ModelTrainer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from src.utils import save_object
+from src.utils import evaluate_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 @dataclass
-class DataIngestionConfig:
-    train_data_path = os.path.join("artifacts/data_ingestion", "train.csv")
-    test_data_path = os.path.join("artifacts/data_ingestion", "test.csv")
-    raw_data_path = os.path.join("artifacts/data_ingestion", "raw.csv")
+class ModelTrainerConfig:
+    train_model_file_apth = os.path.join("artifacts/model_trainer", "model.pkl")
 
-# notbook\data\income_cleandata.csv
-
-class DataIngestion:
+class ModelTrainer:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
-    
-    def inititate_data_ingestion(self):
-        logging.info("Data Ingestion started")
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def inititate_model_trainer(self, train_array, test_array):
         try:
-            logging.info("Data Reading using Pandas library from local system")
-            data = pd.read_csv(os.path.join("notbook/data", "income_cleandata.csv"))
-            logging.info("Data Reading completed")
-
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
-            data.to_csv(self.ingestion_config.raw_data_path, index=False)
-            logging.info("Dat spliteted into train and test")
-
-            train_set, test_set = train_test_split(data, test_size = .30, random_state=42)
-
-            train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
-
-            logging.info("Data Ingestion completed")
-
-            return (
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-
+            logging.info("Splitting our data into dependent and independent features")
+            X_train, y_train, X_test, y_test  = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
             )
+# 
+            model = {
+                "Random Forest": RandomForestClassifier(),
+                "Decision Tree": DecisionTreeClassifier(),
+                "Logastic": LogisticRegression()
+            }
+
+            params = {
+                "Random Forest":{
+                    "class_weight":["balanced"],
+                    'n_estimators': [20, 50, 30],
+                    'max_depth': [10, 8, 5],
+                    'min_samples_split': [2, 5, 10],
+                },
+                "Decision Tree":{
+                    "class_weight":["balanced"],
+                    "criterion":['gini',"entropy","log_loss"],
+                    "splitter":['best','random'],
+                    "max_depth":[3,4,5,6],
+                    "min_samples_split":[2,3,4,5],
+                    "min_samples_leaf":[1,2,3],
+                    "max_features":["auto","sqrt","log2"]
+                },
+                "Logastic":{
+                    "class_weight":["balanced"],
+                    'penalty': ['l1', 'l2'],
+                    'C': [0.001, 0.01, 0.1, 1, 10, 100],
+                    'solver': ['liblinear', 'saga']
+                }
+            }
+
+            model_report:dict = evaluate_model(X_train = X_train, y_train = y_train, X_test = X_test, y_test =y_test,
+                                                models = model, params = params)
+            
+            # To gest best model from our report Dict
+            best_model_score = max(sorted(model_report.values()))
+
+            best_model_name = list(model.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = model[best_model_name]
+            
+            print(f"Best Model Found, Model Name is: {best_model_name},Accuracy_Score: {best_model_score}")
+            print("\n***************************************************************************************\n")
+            logging.info(f"best model found, Model Name is {best_model_name}, accuracy Score: {best_model_score}")
+
+
+            save_object(file_path=self.model_trainer_config.train_model_file_apth,
+                        obj = best_model
+                        )
+
+
+
+
+
+
         except Exception as e:
-            logging.info("Erro occured in data ingestion stage")
             raise CustmeException(e, sys)
-
-if __name__ =="__main__":
-    obj = DataIngestion()
-    treain_data_path , test_data_path = obj.inititate_data_ingestion()
-
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.inititate_data_transformation(treain_data_path , test_data_path)
-
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.inititate_model_trainer(train_arr, test_arr))
-
-
-
-
-
-# src\component\data_ingestion.py
